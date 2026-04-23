c58f13f46fba5730edfe0728dff01b5735f7a4f3
Zach LastName <light48@Marshall.edu>
2026-04-23 12:36:20 -0400

    Add white-box test for accounts login page

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AccountsViewTests(TestCase):
+    def test_login_page_loads(self) -> None:
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/login.html")
