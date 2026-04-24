059006843ea2b601fa558c2701d150a5a64f39fa
Zach LastName <light48@Marshall.edu>
2026-04-23 20:02:41 -0400

    Add additional white-box tests for billing scheduling profiles and records

--- apps/profiles/tests.py
+++ apps/profiles/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from apps.profiles.models.patient_profile import PatientProfile
 from apps.profiles.models.staff_profile import StaffProfile, StaffRole
 
 
@@ -15,3 +16,14 @@ class ProfileModelTests(TestCase):
 
         self.assertEqual(profile.staff_role, StaffRole.ADMIN)
         self.assertTrue(profile.is_active_staff)
+
+
+def test_patient_profile_string_representation(self) -> None:
+    user = get_user_model().objects.create_user(
+        username="patientprofileuser",
+        password="testpass123",
+    )
+
+    profile = PatientProfile.objects.create(user=user)
+
+    self.assertEqual(str(profile), "PatientProfile<patientprofileuser>")
